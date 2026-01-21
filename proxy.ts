c2a5fe4f53bec6a545e44e9775f1dfa595f4bc1e
Abhijit1102 <abhijitrajkumar2@gmail.com>
2026-01-21 21:32:13 +0530

    auth

---
+++ proxy.ts
@@ -0,0 +1,27 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextRequest } from "next/server";
+
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/api(.*)",
+]);
+
+export default clerkMiddleware(async (auth, req: NextRequest) => {
+  if (!isPublicRoute(req)) {
+    await auth.protect();
+  }
+});
+
+export const config = {
+  matcher: [
+    /*
+     * Run middleware on all routes except:
+     * - _next (Next.js internals)
+     * - static files (images, css, js, etc.)
+     */
+    "/((?!_next|.*\\.(?:css|js|json|png|jpg|jpeg|gif|svg|ico|webp|woff2?|ttf)).*)",
+    "/(api|trpc)(.*)",
+  ],
+};
